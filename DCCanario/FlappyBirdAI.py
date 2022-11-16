d205898783fc28f5f9504a94d1cc56109f086813
Vicente Vega Ulloa <vnvega@uc.cl>
2022-11-16 19:41:56 -0300

    Add base code

---
+++ DCCanario/FlappyBirdAI.py
@@ -0,0 +1,298 @@
+from tkinter import W
+import pygame
+import random
+import math
+
+pygame.init()
+
+WIDTH, HEIGHT = 700, 500
+
+FPS = 60
+
+BLUE = (78, 192, 202)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
+CHARACTER_X = WIDTH//2 - 200
+CHARACTER_RADIUS = 20
+CHARACTER_VEL = 2.75
+
+WALL_WIDTH, WALL_HEIGHT = 50, HEIGHT
+WALL_VEL = 3
+
+GRAVITY = 0.125
+
+SCORE_FONT = pygame.font.Font("resources/fb-font.ttf", 80)
+
+SPRITE = pygame.image.load("resources/sprite.png")
+BOT_PIPE = pygame.image.load("resources/bot_pipe.png")
+TOP_PIPE = pygame.image.load("resources/top_pipe.png")
+BACKGROUND = pygame.image.load("resources/background.png")
+FLOOR = pygame.image.load("resources/floor.png")
+
+class Bird:
+    COLOR = WHITE
+
+    def __init__(self, x, y, radius):
+        self.x = self.original_x = x
+        self.y = self.original_y = y
+        self.y += random.uniform(-0.3, 0.3) * HEIGHT
+        self.y_speed = 0
+        self.radius = radius
+
+    def draw(self, win):
+        
+        if self.y_speed != 0:
+            angle = self.y_speed * 45 / (2 * CHARACTER_VEL)
+            if angle < -80:
+                angle = -80
+
+            bird_sprite = pygame.transform.rotate(SPRITE, angle)
+        else:
+            bird_sprite = SPRITE
+
+        win.blit(bird_sprite, (self.x - self.radius, self.y - self.radius))
+
+    def move(self, up = 0):
+        if up == 1:
+            self.y_speed = CHARACTER_VEL
+
+        if self.y + self.radius < HEIGHT:
+            self.y -= self.y_speed
+
+        self.y_speed -= GRAVITY
+
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y + random.uniform(-0.3, 0.3) * HEIGHT
+        self.y_speed = 0
+
+class Wall:
+    COLOR = WHITE
+    WALL_SPEED = WALL_VEL
+    LAST_HOLE = HEIGHT//2
+
+    def __init__(self, x, y, is_second = False):
+        self.x = self.original_x = x
+        self.is_second = is_second
+        if is_second:
+            self.x = 3 * self.original_x//2 + WALL_WIDTH//2
+        self.y = self.original_y = y
+        self.height = WALL_HEIGHT
+        self.width = WALL_WIDTH
+        self.hole_size = CHARACTER_RADIUS * 8
+        self.new_hole = -1
+        self.hole = HEIGHT//2 - int(random.uniform(-0.6, 0.6) * HEIGHT//2)
+        self.score = 35
+
+    def draw(self, win):
+        top_height = self.hole - self.hole_size//2
+        bot_height = self.hole + self.hole_size//2
+        win.blit(TOP_PIPE, (self.x - self.width//2 - 1, -HEIGHT + top_height))
+        win.blit(BOT_PIPE, (self.x - self.width//2 - 1, bot_height))
+
+    def move(self):
+        self.x -= self.WALL_SPEED
+        if self.x < CHARACTER_X - 3 * CHARACTER_RADIUS//2 and self.new_hole < 0:
+            self.compute_new_hole()
+        if self.x < -self.width:
+            self.reset()
+
+    def compute_new_hole(self):
+        self.new_hole = HEIGHT//2 - int(random.uniform(-0.6, 0.6) * HEIGHT//2)
+
+        max_dist_top = ((WIDTH//2)/Wall.WALL_SPEED) * CHARACTER_VEL * 0.7
+
+        if Wall.LAST_HOLE - self.new_hole > max_dist_top:
+            self.new_hole = Wall.LAST_HOLE - max_dist_top
+
+        max_dist_bot = ((WIDTH//2)/Wall.WALL_SPEED) * GRAVITY * FPS * 0.5
+        if self.new_hole - Wall.LAST_HOLE > max_dist_bot:
+            self.new_hole = Wall.LAST_HOLE + max_dist_bot
+
+        Wall.LAST_HOLE = self.new_hole
+        self.score += 1
+
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+        Wall.WALL_SPEED = WALL_VEL + WALL_VEL * self.score/100
+        self.hole = self.new_hole
+        self.new_hole = -1
+        hole_size = CHARACTER_RADIUS * 8 - self.score//1.5
+        self.hole_size = hole_size if hole_size > CHARACTER_RADIUS * 1.05 else CHARACTER_RADIUS * 1.05
+
+    def reset_original(self):
+        Wall.WALL_SPEED = WALL_VEL
+        self.__init__(self.original_x, self.original_y, self.is_second)
+
+class FlappyBird():
+    def __init__(self, vis = True):
+        self.vis = vis
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.frame_iteration = 0
+        self.floor_x = 0
+
+        if self.vis:
+            self.window = pygame.display.set_mode((WIDTH, HEIGHT + 50))
+            pygame.display.set_caption("Flappy Bird")
+            self.clock = pygame.time.Clock()
+
+        self.character = Bird(CHARACTER_X, HEIGHT//2, CHARACTER_RADIUS)
+        self.walls = [Wall(WIDTH, 0), Wall(WIDTH, 0, True)]
+        self.score = 0
+        self.current_wall = self.walls[0]
+
+    def draw(self):
+        self.window.fill(BLUE)
+        self.window.blit(BACKGROUND, (0, 0))
+
+        self.character.draw(self.window)
+
+        for wall in self.walls:
+            wall.draw(self.window)
+
+        score_text = SCORE_FONT.render(f"{self.score}", 1, WHITE)
+        shadow_text = SCORE_FONT.render(f"{self.score}", 1, BLACK)
+
+        self.window.blit(shadow_text, (WIDTH//2 - score_text.get_width()//2 + 5, 20 + 5))
+        self.window.blit(score_text, (WIDTH//2 - score_text.get_width()//2, 20))
+        
+        self.window.blit(FLOOR, (self.floor_x, HEIGHT))
+
+        pygame.display.update()
+
+    def move(self, action):
+        self.character.move(action)
+
+        for wall in self.walls:
+            wall.move()
+        
+        self.floor_x -= Wall.WALL_SPEED
+        if self.floor_x < -432:
+            self.floor_x = 0
+
+    def handle_collision(self):
+
+        collision = False
+
+        if self.character.y + self.character.radius >= HEIGHT or self.character.y < 0:
+            return True
+
+        for wall in self.walls:
+            if wall.x - WALL_WIDTH//2 >= CHARACTER_X + CHARACTER_RADIUS or wall.x + WALL_WIDTH//2 <= CHARACTER_X - CHARACTER_RADIUS:
+                continue
+
+            if wall.x - WALL_WIDTH//2 <= CHARACTER_X and wall.x + WALL_WIDTH//2 >= CHARACTER_X:
+                if self.character.y - CHARACTER_RADIUS < wall.hole - wall.hole_size//2 or self.character.y + CHARACTER_RADIUS > wall.hole + wall.hole_size//2:
+                    collision = True
+                continue
+
+            if wall.x + WALL_WIDTH//2 < CHARACTER_X:
+                delta_x = CHARACTER_X - (wall.x + WALL_WIDTH//2)
+                dist_y_top = math.sqrt(delta_x**2 + (wall.hole - wall.hole_size//2 - self.character.y)**2)
+                dist_y_bot = math.sqrt(delta_x**2 + (wall.hole + wall.hole_size//2 - self.character.y)**2)
+                if dist_y_top < CHARACTER_RADIUS or dist_y_bot < CHARACTER_RADIUS:
+                    collision = True
+                continue
+
+            if wall.x - WALL_WIDTH//2 > CHARACTER_X:
+                delta_x = wall.x - WALL_WIDTH//2 - CHARACTER_X
+                dist_y_top = math.sqrt(delta_x**2 + (wall.hole - wall.hole_size//2 - self.character.y)**2)
+                dist_y_bot = math.sqrt(delta_x**2 + (wall.hole + wall.hole_size//2 - self.character.y)**2)
+                if dist_y_top < CHARACTER_RADIUS or dist_y_bot < CHARACTER_RADIUS:
+                    collision = True
+                continue
+
+        return collision
+
+    def compute_score(self):
+        score = 0
+        for wall in self.walls:
+            score += wall.score
+        return score
+
+    def reset(self):
+        self.frame_iteration = 0
+        self.score = 0
+
+        self.character.reset()
+        for wall in self.walls:
+            wall.reset_original()
+        self.current_wall = self.walls[0]
+        if self.vis:
+            self.draw()
+
+    def play_step(self, action):
+
+        # Ejecuta una acción elegida por el agente previamente
+
+        if self.vis:
+            # Actualiza la interfaz en caso de haber visualización
+            self.draw()
+            self.clock.tick(FPS)
+
+        game_over = False
+        self.frame_iteration += 1
+
+        if self.vis:
+            # Revisa si el usuario ha cerrado la ventana
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+        self.score = self.compute_score()
+        past_score = self.score
+
+        # Mueve los componentes del juego en el espacio
+        self.move(action)
+
+        if self.walls[1].score < self.walls[0].score:
+            self.current_wall = self.walls[1]
+        else:
+            self.current_wall = self.walls[0]
+
+        # En caso de golpear a la pelota
+        if self.handle_collision():
+            game_over = True
+            self.reset()
+        
+        reward = -1
+
+        # Revisa si el agente se encuentra encima o sobre el agujero
+        if self.current_wall.hole >= self.character.y:
+
+            # Recompensar al agente si no salta, castigarlo si no
+            if action == 0:
+                reward = 1
+            else:
+                reward = -1
+
+        # Si el agente esta debajo del agujero
+        else:
+            # Recompensar al agente si se mueve hacia abajo, castigarlo si no
+            if action == 1:
+                reward = 1
+            else:
+                reward = -1
+
+        if self.compute_score() > past_score:
+            reward = 50
+
+        if game_over:
+            reward = -1
+
+        # Retornar información al agente
+        return reward, game_over, past_score
+
+def wait():
+    while True:
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            return
+        elif event.type == pygame.KEYDOWN:
+            return
---
+++ DCCanario/QAgent.py
@@ -0,0 +1,162 @@
+import random
+import numpy as np
+from FlappyBirdAI import FlappyBird
+
+# Hiperparámetros
+LR = 0.3
+NUM_EPISODES = 10_000_000
+DISCOUNT_RATE = 0
+MAX_EXPLORATION_RATE = 1
+MIN_EXPLORATION_RATE = 0.0002
+EXPLORATION_DECAY_RATE = 0.00015
+
+VISUALIZATION = False
+
+class Agent:
+    # Esta clase posee al agente y define sus comportamientos.
+
+    def __init__(self):
+        # Creamos la q_table y la inicializamos en 0.
+        # IMPLEMENTAR
+        self.q_table = None
+
+        # Inicializamos los juegos realizados por el agente en 0.
+        self.n_games = 0
+
+        # Inicializamos el exploration rate.
+        self.EXPLORATION_RATE = MAX_EXPLORATION_RATE
+
+        # Inicializamos los juegos realizados por el agente en 0.
+        self.n_games = 0
+        
+        # Inicializamos el exploration rate.
+        self.EXPLORATION_RATE = MIN_EXPLORATION_RATE + (MAX_EXPLORATION_RATE - MIN_EXPLORATION_RATE) * np.exp(-EXPLORATION_DECAY_RATE*self.n_games)
+
+    def get_state(self, game):
+        # Este método consulta al juego por el estado del agente y lo retorna como una tupla.
+        state = []
+
+        # Computamos la distancia en el eje x entre el jugador y la tubería más cercana
+        delta_x = game.current_wall.x - game.character.x
+        delta_x = delta_x//22
+        if delta_x < 0:
+            delta_x = 0
+        if delta_x > 50:
+            delta_x = 50
+        state.append(delta_x)
+
+        sense = 0
+
+        # Calculamos la distancia en el eje y con el agujero de la próxima tubería
+        delta_y = game.character.y - game.current_wall.hole
+        if delta_y < 0:
+            # Sense nos indica si la tubería se encuentra encima o debajo del agente
+            sense = 1
+        delta_y = abs(delta_y//15)
+        if delta_y > 27:
+            delta_y = 27
+        state.append(int(delta_y))
+        state.append(sense)
+
+        sense_next = 0
+
+        # Calculamos la distancia al agujero de la tubería que vendrá después de la actual
+        delta_y_next = game.character.y - game.current_wall.hole
+        if delta_y_next < 0:
+            # Sense nos indica si el agujero se encontrará encima o debajo del agente
+            sense_next = 1
+        delta_y_next = abs(delta_y_next//15)
+        if delta_y_next > 27:
+            delta_y_next = 27
+        state.append(int(delta_y_next))
+        state.append(sense_next)
+
+        return tuple(state)
+
+    def get_action(self, state):
+        # Este método recibe una estado del agente y retorna un entero con el indice de la acción correspondiente.
+
+        # IMPLEMENTAR
+
+        return random.choice([0, 1])
+
+def train():
+    # Esta función es la encargada de entrenar al agente.
+
+    # Las siguientes variables nos permitirán llevar registro del desempeño del agente.
+    plot_scores = []
+    plot_mean_scores = []
+    mean_score = 0
+    total_score = 0
+    record = 0
+    period_steps = 0
+    period_score = 0
+
+    # Instanciamos al agente o lo cargamos desde un pickle.
+    agent = Agent()
+
+    # Instanciamos el juego. El bool 'vis' define si queremos visualizar el juego o no.
+    # Visualizarlo lo hace mucho más lento.
+    game = FlappyBird(vis = VISUALIZATION)
+
+    # Inicializamos los pasos del agente en 0.
+    steps = 0
+
+    while True:
+        # Obtenemos el estado actual.
+        state = agent.get_state(game)
+        # Generamos la acción correspondiente al estado actual.
+        move = agent.get_action(state)
+        # Ejecutamos la acción.
+        reward, done, score = game.play_step(move)
+
+        # Obtenemos el nuevo estado.
+        # IMPLEMENTAR
+
+        # Actualizamos la q-table.
+        # IMPLEMENTAR
+
+        # En caso de terminar el juego.
+        if done:
+            # Actualizamos el exploration rate.
+            # IMPLEMENTAR
+
+            # Reiniciamos el juego.
+            game.reset()
+
+            # Actualizamos los juegos jugados por el agente.
+            agent.n_games += 1
+
+            # Imprimimos el desempeño del agente cada 100 juegos.
+            if agent.n_games % 100 == 0:
+                # La siguiente línea guarda la QTable en un archivo (para poder ser accedida posteriormente)
+                np.save("q_table.npy", agent.q_table)
+
+                # Información relevante sobre los últimos 100 juegos
+                print('Game', agent.n_games, 'Mean Score', period_score/100, 'Record:', record, "EXP_RATE:", agent.EXPLORATION_RATE, "STEPS:", period_steps/100)
+                record = 0
+                period_score = 0
+                period_steps = 0
+
+            # Actualizamos el record del agente.
+            if score > record:
+                record = score
+            
+            # Actualizamos nuestros indicadores.
+            period_steps += steps
+            period_score += score
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            steps = 0
+            
+            # En caso de alcanzar el máximo de juegos cerramos el loop. 
+            if agent.n_games == NUM_EPISODES:
+                break
+
+        else:
+            steps += 1
+
+if __name__ == '__main__':
+    train()
